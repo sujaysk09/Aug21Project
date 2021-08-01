576c5e51ed5c8933b9229daf38e0500f6992eb7b
Admin <Admin@Lenovo-PC>
2021-08-01 14:17:30 +0530

    [sujay] Created demo1

---
+++ src/Practice/demo1.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class demo1 {
+
+	public static void main(String[] args) 
+	{
+		String str="i am \"sujay\"";
+		System.out.println(str);
+		
+		System.out.println("i am \"sujay\"");
+	}
+
+}
